545a450ae173300dc5ba130aec9fe7518d3c814a
Khilan9 <59089510+Khilan9@users.noreply.github.com>
2020-09-19 23:58:03 +0530

    Add files via upload

--- Django_channels_chat/chat/views.py
+++ Django_channels_chat/chat/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
+from django.contrib.sessions.models import Session
 
 # Create your views here.
 def index(request):
@@ -8,6 +11,12 @@ def chatselect(request):
     return render(request,"chat/chatbutton.html")
 
 def room(request, room_name='a'):
+    request.session['sessionuserid']=request.POST['id']
+    request.session['sessionusernme']=request.POST['username']
+    print(request.session['sessionuserid'])
+    print(request.session['sessionusernme'])
     return render(request, 'chat/room.html', {
-        'room_name': 'a'
+        'room_name': 'a',
+        'username': mark_safe(json.dumps(request.session['sessionusernme'])),
+        'idofuser': mark_safe(json.dumps(request.session['sessionuserid'])),
     })
